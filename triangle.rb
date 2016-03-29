40cb0422fd3e331998a47f9d8c48dd2640df8633
Jason Halbur (Pillar) <jhalbur@hal2000.Home>
2016-03-29 11:53:24 -0500

    Ruby Tuesday 161 3-29-2016

--- triangle.rb
+++ triangle.rb
@@ -13,17 +13,43 @@
 # and
 #   about_triangle_project_2.rb
 #
+
+
+def check_equilateral?(a,b,c)
+  a == b && b == c && a == c
+end
+
+def check_isosceles?(a,b,c)
+  (a == b || a == c) || (b == c || b == a)
+end
+
+def check_scalene?(a, b, c)
+  a != b && b != c && a != c
+end
+
 def triangle(a, b, c)
   # WRITE THIS CODE
 
-  if(a == b && b == c)
+  sides = [a,b,c].sort
+  c = sides.pop
+  b = sides.pop
+  a = sides.pop
+
+  if (a < 1 || b < 1 || c < 1) || (a + b <= c)
+    raise TriangleError
+  end
+
+  if (check_equilateral?(a,b,c))
     return :equilateral
-  elsif(a == b || b == c || a == c)
+  elsif (check_isosceles?(a,b,c))
     return :isosceles
-  else
+  elsif (check_scalene?(a,b,c))
     return :scalene
+  else
+    raise TriangleError
   end
 end
+
 
 # Error class used in part 2.  No need to change this code.
 class TriangleError < StandardError
